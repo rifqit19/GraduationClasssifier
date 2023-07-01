693027e692d2df9d6e1af79e577ae5f62b072459
rifqit19 <rifqit19@gmail.com>
2023-07-01 23:35:45 +0700

    Initial Commit

---
+++ GraduationClassifier/GraduationClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraduationClassifierApp.swift
+//  GraduationClassifier
+//
+//  Created by rifqi triginandri on 01/07/23.
+//
+
+import SwiftUI
+
+@main
+struct GraduationClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
